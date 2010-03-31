11c225e636e0064384ab8b5d6219af78342e992f
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2010-03-30 23:29:45 -0400

    init commit

---
+++ src/com/appspot/antares/client/Antares.java
@@ -0,0 +1,17 @@
+package com.appspot.antares.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Antares implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
